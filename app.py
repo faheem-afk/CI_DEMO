19c5a1545579e4503b78e5a7203f674f1a6ae60c
= <adahm7114@gmail.com>
2025-07-26 04:06:22 +0100

    add mod

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ def div(a, b):
     return a/b
 
 
+def mod(a):
+    if a < 0:
+        return a * -1
+    else:
+        return a
+
+
 result1 = add(4, 5)
 result2 = sub(4, 1)
 print(result1, result2)
