fff098f43a8d402330fee233449e4959e2a0807c
= <adahm7114@gmail.com>
2025-07-26 04:17:10 +0100

    root3

---
+++ app.py
@@ -0,0 +1,38 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def div(a, b):
+    return a/b
+
+
+def mod(a):
+    if a < 0:
+        return a * -1
+    else:
+        return a
+
+
+def mul(a, b):
+    return a * b
+
+
+def root(a):
+    return a**1/2
+
+
+def root2(a):
+    return a**1/2
+
+
+def root3(a):
+    return a**1/2
+
+
+result1 = add(4, 5)
+result2 = sub(4, 1)
+print(result1, result2)
